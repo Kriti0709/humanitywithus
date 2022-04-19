af18458845afbf82936cd4a51ca2dc35f079b1fb
Kriti0709 <kritiveer0709@gmail.com>
2022-04-20 00:18:08 +0530

    adding script for heroku

--- hmu/database.js
+++ hmu/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect("mongodb+srv://kritika_0709:<Kritika_0709>@cluster0.apz3n.mongodb.net/krtika_0709?retryWrites=true&w=majority/contact")
+    const connect = await mongoose.connect("mongodb+srv://kritika_0709:<Kritika_0709>@cluster0.apz3n.mongodb.net/contact")
     console.log("DB connected:", connect.connection.host)
   } catch (error) {
     console.log(error)
